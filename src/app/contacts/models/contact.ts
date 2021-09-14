4e3c2beb3a2e1caff10237d4d1ef0bd27cc3b0ea
Valeriia Lashkul <valeriia.lashkul@lizard-soft.com>
2021-09-15 02:06:26 +0300

    initial commit

---
+++ src/app/contacts/models/contact.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+	firstName: string;
+	lastName: string;
+	middleName: string;
+	organization: string;
+	department: string;
+	position: string;
+	email: string;
+	phone: string;
+}
---
+++ src/app/contacts/services/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable, BehaviorSubject } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+import { Contact } from '../models/contact';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ApiService {
+
+	private contacts: BehaviorSubject<Contact[]> = new BehaviorSubject([]);
+	public contacts$: Observable<Contact[]> = this.contacts.asObservable().pipe(delay(200));
+	constructor() {
+		const contactsArray = new Array(15).fill(SAMPLE_CONTACT);
+		this.contacts.next(contactsArray);
+	}
+}
+
+const SAMPLE_CONTACT: Contact = {
+	firstName: 'Иван',
+	lastName: 'Иванов',
+	middleName: 'Иванович',
+	organization: 'ООО "Ромашки"',
+	department: 'Главное отделение',
+	position: 'Эксперт',
+	email: 'ivan.ivanov@romashki.com',
+	phone: '+380443456789'
+};
